e89fab4e7aad38ba36d504519a22924052dfea7c
Tobe Ekanem <ekanemwork@gmail.com>
2024-03-13 14:51:28 +0100

    update

--- src/app/private-module/dashboard/dashboard.component.ts
+++ src/app/private-module/dashboard/dashboard.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+
+  match_feed: any[]=[];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+    
+  }
 
 }
