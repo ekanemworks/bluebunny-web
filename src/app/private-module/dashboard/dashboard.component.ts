5c1c832f339c765549f0bf85031a593023964442
Tobe Ekanem <ekanemwork@gmail.com>
2024-03-14 19:28:26 +0100

    update

--- src/app/private-module/dashboard/dashboard.component.ts
+++ src/app/private-module/dashboard/dashboard.component.ts
@@ -9,10 +9,82 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
 
-  match_feed: any[]=[];
+  match_feed_list: any[]=[];
+  active_feed: any;
+  activeIndex: number = 0;
+
+  constructor() {}
+
 
   ngOnInit(): void {
 
+    this.matchFeed();
+
   }
+
+
+  matchFeed(){
+    this.match_feed_list = [
+      {
+        profileName: "John Doe",
+        imageArray: "['../../assets/test-img.jpeg']",
+        dateOfBirth:"12-10-1999"
+      },
+      {
+        profileName: "John Doe",
+        imageArray: "['../../assets/test-img2.jpeg']",
+        dateOfBirth:"12-10-1999"
+      },
+      {
+        profileName: "John Doe",
+        imageArray: "['../../assets/test-img3.png']",
+        dateOfBirth:"12-10-1999"
+      }
+    ]
+
+
+    this.match_feed_list = this.randomizeFeed(this.match_feed_list);
+    this.active_feed = this.match_feed_list[this.activeIndex];
+  }
+
+  // Randomizes the order of images in  each feed item
+  randomizeFeed<T>(array: T[]): T[] {
+      let currentIndex = array.length;
+      let temporaryValue: T;
+      let randomIndex: number;
+  
+      // While there remain elements to shuffle...
+      while (currentIndex !== 0) {
+  
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex--;
+  
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+      }
+  
+      return array;
+    
+  }
+
+
+  like_feed(){
+
+  }
+
+
+  dislike_feed(){
+
+  }
+
+  initiateChat(){
+    
+  }
+
+
+
 
 }
