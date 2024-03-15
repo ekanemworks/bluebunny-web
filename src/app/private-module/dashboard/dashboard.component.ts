338a69b20ac0768116a43236eb8a1b17cff7a360
Tobe Ekanem <ekanemwork@gmail.com>
2024-03-15 12:03:27 +0100

    update

--- src/app/private-module/dashboard/dashboard.component.ts
+++ src/app/private-module/dashboard/dashboard.component.ts
@@ -13,6 +13,11 @@ export class DashboardComponent implements OnInit {
   active_feed: any;
   activeIndex: number = 0;
 
+  choice = {
+    like: false,
+    dislike: false
+  }
+
   constructor() {}
 
 
@@ -27,24 +32,36 @@ export class DashboardComponent implements OnInit {
     this.match_feed_list = [
       {
         profileName: "John Doe",
-        imageArray: "['../../assets/test-img.jpeg']",
-        dateOfBirth:"12-10-1999"
+        imageArray: ['../../assets/test-img.jpeg'],
+        dateOfBirth:"12-10-1997",
+        stateName:'Lagos'
       },
       {
-        profileName: "John Doe",
-        imageArray: "['../../assets/test-img2.jpeg']",
-        dateOfBirth:"12-10-1999"
+        profileName: "Tobe Ekanem",
+        imageArray: ['../../assets/test-img2.jpeg'],
+        dateOfBirth:"12-10-1996",
+        stateName:'Ogun'
       },
       {
-        profileName: "John Doe",
-        imageArray: "['../../assets/test-img3.png']",
-        dateOfBirth:"12-10-1999"
+        profileName: "Raphael",
+        imageArray: ['../../assets/test-img3.jpeg'],
+        dateOfBirth:"12-10-1999",
+        stateName:'Abuja'
       }
     ]
 
 
     this.match_feed_list = this.randomizeFeed(this.match_feed_list);
     this.active_feed = this.match_feed_list[this.activeIndex];
+  }
+
+  splitImg(imageArrString:any){
+    console.log(imageArrString);
+    
+    // let imgArr =  JSON.parse(imageArrString)
+    // console.log(imgArr);
+    // let img = imgArr[0];
+    return imageArrString[0];
   }
 
   // Randomizes the order of images in  each feed item
@@ -72,17 +89,68 @@ export class DashboardComponent implements OnInit {
 
 
   like_feed(){
-
-  }
+    this.choice.like = true
+    let seconds = 1
+    let timer = setInterval(() => {
+      seconds--;
+      if (seconds < 0) {
+          clearInterval(timer);
+          this.nextAgent();
+      }
+    }, 500);
+  } 
 
 
   dislike_feed(){
-
+    this.choice.dislike=true
+    let seconds = 1
+    let timer = setInterval(() => {
+      seconds--;
+      if (seconds < 0) {
+          clearInterval(timer);
+          this.nextAgent();
+      }
+    }, 500);
   }
 
   initiateChat(){
     
   }
+
+
+  nextAgent(){
+    this.choice.dislike = false;
+    this.choice.like = false;
+    this.activeIndex++
+    if ((this.match_feed_list.length)>this.activeIndex) {
+      this.active_feed = this.match_feed_list[this.activeIndex];  
+    } else {
+      this.activeIndex = 0;
+      this.active_feed = this.match_feed_list[this.activeIndex];  
+
+      // this.alertMessage.create('warn','Wait for 3 minutes')
+    }
+  }
+
+
+  calculateAgeFromDOB(date_of_birth:any){
+    // Parse the input date string into a Date object
+   const dob: Date = new Date(date_of_birth);
+   
+   // Get the current date
+   const currentDate: Date = new Date();
+
+   // Calculate the difference in years between current date and birth year
+   const age: number = currentDate.getFullYear() - dob.getFullYear();
+
+   // Check if the birthday has occurred this year or not
+   const hasBirthdayOccurred: boolean =
+     currentDate.getMonth() > dob.getMonth() ||
+     (currentDate.getMonth() === dob.getMonth() && currentDate.getDate() >= dob.getDate());
+
+   // If birthday hasn't occurred this year, subtract 1 from the age
+   return hasBirthdayOccurred ? age : age - 1;
+ }
 
 
 
